49e6bca13c13a3afa613e76d3ef5cf6e9562a6ea
Renan Procopio Duarte <rpduarte@firjan.com.br>
2025-10-02 21:38:35 -0300

    orm com fk

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,6 @@
-carro1
+with Session() as session:
+    resultado = session.query(Aluno).all()
+
+    for aluno in resultado:
+        print(aluno.id, aluno.nome, \
+              aluno.idade, aluno.email)
